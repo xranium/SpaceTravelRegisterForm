ff0ab31b9498eb8195dbf2d15175931a144017e0
xranium <shahriar.fun2005@gmail.com>
2025-10-11 13:06:44 +0330

    initialize

---
+++ GroupProjectBackend/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace GroupProjectBackend.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
